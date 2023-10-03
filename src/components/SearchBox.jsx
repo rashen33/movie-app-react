8cf934a86fa8cbff3d7c2f94f1de01bb0f7d8e43
ZainabMalik5 <95160741+ZainabMalik5@users.noreply.github.com>
2023-10-04 01:45:15 +0530

    Update SearchBox.jsx

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import {UilSearch} from '@iconscout/react-unicons'
 const SearchBox = (props) => {
   return (
     <div className="p-4">
       <input className="py-2" type="text" value={props.value} onChange={(event) => props.setSearchValue(event.target.value)} placeholder="Serach a movie" />
+      <div className="s-icon">
+               <UilSearch/>
+           </div>
     </div>
   );
 };
